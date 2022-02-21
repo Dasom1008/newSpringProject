2548fbdf687566856f6c73dc7ac8da2e8d825fed
Dasom1008 <heaven10087@gmail.com>
2022-02-21 17:00:44 +0900

    02.21 jdbcConnection 수정 db연결 진행중

--- src/main/java/SQLConnection/JDBCConnection.java
+++ src/main/java/SQLConnection/JDBCConnection.java
@@ -1,40 +1,28 @@
 package SQLConnection;
 
+
+import org.testng.annotations.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class JDBCConnection {
-    public static void main(String[] args) {
-        Connection con = null;
-
-        String server = "localhost";         //SQL 서버주소
-        String database = "";
-        String user_name = "";
-        String password = "1234";
-
-        //1. 드라이버 로딩
+    static {
         try {
-            Class.forName("com.sql.jdbc.Driver");
-        }catch (ClassNotFoundException e){
-            System.err.println("!!<JDBC오류> Driver load 오류"+e.getMessage());
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        }catch (Exception e){
             e.printStackTrace();
         }
 
-        //2. 연결
-        try {
-            con = DriverManager.getConnection("jdbc:sql://"+server+"/"+database+"?useSSL=false", user_name, password);
-            System.out.println("정상적으로 연결되었습니다.");
-        } catch (SQLException e){
-            System.err.println("con 오류" + e.getMessage());
-            e.printStackTrace();
-        }
-
-        //3. 해제
-        try {
-            if(con != null){
-                con.close();
-            }
-        }catch (SQLException e){}
     }
+    @Test
+    public void testConnection(){
+        try (Connection con = DriverManager.getConnection("jdbc:oracel:thin:@localhost:1521:orcl","test","1234")){
+            log.info(con);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
 }
